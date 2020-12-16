e4be36686c67b738c4fa7b737e2fb18bf4e28967
AustinHoschar <70162263+AustinHoschar@users.noreply.github.com>
2020-12-16 08:18:47 -0500

    first commit

---
+++ JS Output Challenge/script.js
@@ -0,0 +1,36 @@
+// Enter your code below to edit index.html
+
+yourName = "Austin Hoschar"
+document.getElementById("yourName").innerHTML = yourName;
+
+myBio = "I'm Austin Hoschar and I'm learning how to code Javascript right now and its really annoying but thats because I have problems spelling things right but thats normal for me and my only real hobbies are working out and playing video games but sadly I don't have anyone to workout with so I don't do it that much because things are more funny when you do it with friends. I used to play soccer but I stopped playing soccer in highschool because everyone took it too serious and I just thought of it as a funny game to play when I was bored at home I got to play for 9 years so I got my fun out of it.I found out that I may be a little Autophobic sometimes but that is something I need to work on."
+document.getElementById("myBio").innerHTML = myBio;
+
+collegeChoice = "Actually I don't even know if I want to go to collage."
+document.getElementById("collegeChoice").innerHTML = collegeChoice;
+
+programChoice = "I don't really plan ahead of schedule so I just never really knew if I even wanted to go to college at all or even a trade school."
+document.getElementById("programChoice").innerHTML = programChoice;
+
+
+collegeReason = "Most people would give you some long reasons of how it's 'super expensive' and everything but I just don't know what to do with anything and I can never decide on most things so I normally ask someone else what I should do but for some reason sometimes I just think that's cool and than my brain's says not for me and forgets even if I think that sounds reasonable but I still forget. I'm not really thinking about the future but I think I should really sit down and think about it one of these years. Most people disagree with me but I don't think of myself as a even remotely smart people but my teachers all disagree with me but I think its just because it's their job to at least try to be nice and encourage us a little bit but I think they should be a little more straightforward in highschool but they decide to sugar coat a lot of what they then they say that you suddenly decide what your doing with your life because the school tells them to more or less 'play nice with the kids and teach them all of the stuff to pass' but than at least some tell you how it is but they normally get fired or in trouble."
+document.getElementById("collegeReason").innerHTML = collegeReason;
+
+careerChoice = "Probably Coding or just Software Development"
+document.getElementById("careerChoice").innerHTML = careerChoice ;
+
+companyChoice = "I would have to pick Microsoft"
+document.getElementById("companyChoice").innerHTML =companyChoice ;
+
+careerReason = "I picked them because they are one of the biggest companys and I needed to put something down and I really don't any ideas on what to do yet."
+document.getElementById("careerReason").innerHTML = careerReason;
+
+
+answerOne = "Document Object Model is a host for stuff like XML and HTML and reats them like a tree and it uses everything they give it to make a logical tree thats is made up of everything in that tree that makes that tree broken down into parts and borken down into more parts that goes until the end or base of the tree which will have the bare components."
+document.getElementById("answerOne").innerHTML = answerOne ;
+
+answerTwo = "It can write into an HTML element, it can write into the output using document.write() and it can write also can cause a alert on your tab while it writes in the browser console using console.log()"
+document.getElementById("answerTwo").innerHTML = answerTwo ;
+
+answerThree = "You can change the text as long as you put a id on it than it can be edited in javascript you can also also make a if else statement for if this happens do this to the html."
+document.getElementById("answerThree").innerHTML = answerThree; 
